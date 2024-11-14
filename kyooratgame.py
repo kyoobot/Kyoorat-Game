79cb54f7264dac8e35f357db9b39b4a84a4379d4
KB <bernadetteaperez1994@gmail.com>
2024-11-14 00:34:07 -0600

    Adding controls to move player

--- kyooratgame.py
+++ kyooratgame.py
@@ -16,35 +16,45 @@ BLUE = (0,0,255)
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder,"sprites")
 
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width,screen_height))
+
 # making a class for the Player
 class Player(pygame.sprite.Sprite):
     # sprite for the player
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join(img_folder,"testsprite.png"))
+        # I will stick with convert_alpha for now, but i will keep the convert/ color key method in mind in case i need it
+        self.image = pygame.image.load(os.path.join(img_folder,"testsprite.png")).convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.center = (screen_width / 2, screen_height / 2)
+        self.rect.centerx = screen_width/2
+        self.rect.bottom = screen_height - 10
+        self.speedx = 0 
     def update(self):
-        self.rect.x += 5
-        if self.rect.left > screen_width:
-            self.rect.right = 0
+        self.speedx = 0 
+        # looking for keypresses to move! speedx = 0 makes sure sprite doesnt move anymore once key isnt pressed
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        self.rect.x += self.speedx
 
 pygame.init()
 pygame.display.set_caption('Kyoorat Game')
-screen_width = 800
-screen_height = 600
+
 #new icon
 rat_icon = pygame.image.load(os.path.join(img_folder,"kyooraticon.png"))
 pygame.display.set_icon(rat_icon)
 pygame.mixer.init()
 clock = pygame.time.Clock()
-fps = 30
+fps = 60
 all_sprites = pygame.sprite.Group()
 # player = class Player
 player = Player()
 all_sprites.add(player)
 
-screen = pygame.display.set_mode((screen_width,screen_height))
 
 # Game Loop
 running = True
@@ -60,7 +70,7 @@ while running == True:
     #update
     all_sprites.update()
     #draw/render
-    screen.fill(BLACK)
+    screen.fill(BLUE)
     all_sprites.draw(screen)
 
     #after drawing everything flip the display
